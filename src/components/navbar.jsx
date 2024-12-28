6aa44f043adac0420d427ad18d569c09d4d2e5b0
Taranjeet Singh Bedi <taranjeetsinghbedi2@gmail.com>
2024-12-28 23:38:26 +0530

    Added: About

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -44,12 +44,50 @@ export default function Navbar() {
       </div>
       <div className="navbarright">
         <ul>
-          <li onClick={() => navigate("/")}>Home</li>
-          <li onClick={() => navigate("/about")}>About</li>
-          <li>Tax Advisory</li>
-          <li>Work Permits</li>
-          <li>Immigration Stamps</li>
-          <li>Company Registration </li>
+          <li>Home</li>
+          <li>About</li>
+          <li>
+            Tax Advisory
+            <ul className="dropdown-menu">
+              <li onClick={() => navigate("/form11")}>Form 11</li>
+              <li onClick={() => navigate("/form12")}>Form 12</li>
+              <li onClick={() => navigate("/financial-advisory")}>
+                Financial Advisory
+              </li>{" "}
+              <li onClick={() => navigate("/pensions-advisory")}>
+                Pensions Advisory
+              </li>
+            </ul>
+          </li>
+          <li>
+            Work Permits{" "}
+            <ul className="dropdown-menu">
+              <li onClick={() => navigate("/csep")}>CSEP</li>
+              <li onClick={() => navigate("/gep")}>GEP</li>
+              <li onClick={() => navigate("/stamp-1g")}>Stamp 1G Extension</li>
+            </ul>
+          </li>
+          <li>
+            Immigration Stamps{" "}
+            <ul className="dropdown-menu">
+              <li onClick={() => navigate("/stamp-0")}>Stamp 0</li>
+              <li onClick={() => navigate("/vfs-application-support")}>
+                VFS Application Support
+              </li>
+              <li onClick={() => navigate("/stamp-5")}>Stamp 5</li>
+              <li onClick={() => navigate("/stamp-6")}>Stamp 6</li>
+              <li onClick={() => navigate("/adult-naturalisation")}>
+                Adult Naturalisation
+              </li>
+              <li onClick={() => navigate("/child-naturalisation")}>
+                Child Naturalisation
+              </li>
+              <li onClick={() => navigate("/citizenship-by-birth")}>
+                Citizenship by Birth
+              </li>
+            </ul>
+          </li>
+          <li>Company Registration</li>
           <li>
             <button onClick={() => navigate("/contact")}>Contact Us</button>
           </li>
